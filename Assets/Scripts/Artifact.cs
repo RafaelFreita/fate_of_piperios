cd449dc478406744f6f0e0c0e4133f5a03efb010
Rafael de Freitas <jamerrf13@unisinos.br>
2019-10-31 13:03:32 -0300

    Adding planet and artifact; Dumb logging lol

---
+++ Assets/Scripts/Artifact.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Artifact : MonoBehaviour
+{
+
+    public StoryNode node;
+
+    private bool activatable = true;
+    private TextMeshProUGUI textLog;
+
+    private void Start()
+    {
+        textLog = GameObject.FindGameObjectWithTag("HistoryLog").GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Activate()
+    {
+        // Não ser clicável caso o mundo não acabe nesse path
+        if (!activatable) return;
+
+        // Ativa o nodo
+        node.isActive = true;
+
+        // Mostra a mensagem
+        textLog.text = node.message;
+        
+        // Checa se possui end rule
+        if (node.endRule)
+        {
+            if (node.endRule.Check()) // Mundo acabou
+            {
+                textLog.text = node.endRule.triggeredMessage;
+            }
+            else                      // Mundo não acabou
+            {
+                textLog.text = node.endRule.untriggeredMessage;
+                activatable = false;
+            }
+        }
+
+        // Passa pro proximo nodo
+        if (node.nextNode)
+            node = node.nextNode;
+        else
+            Debug.LogError("Didn't find next node!! Title: " + node.title);
+
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Clicked on artifact: " + name);
+        Activate();
+    }
+
+}
--- Assets/Scripts/Data/EndRule.cs
+++ Assets/Scripts/Data/EndRule.cs
@@ -5,8 +5,24 @@ using UnityEngine;
 [System.Serializable, CreateAssetMenu(fileName = "EndRule")]
 public class EndRule : ScriptableObject
 {
-    public string message;
+    public string triggeredMessage;
+    public string untriggeredMessage;
 
     public List<StoryNode> activeNodes;
     public List<StoryNode> unactiveNodes;
+
+    public bool Check()
+    {
+        foreach(StoryNode node in activeNodes)
+        {
+            if (!node.isActive) return false;
+        }
+
+        foreach (StoryNode node in unactiveNodes)
+        {
+            if (node.isActive) return false;
+        }
+
+        return true;
+    }
 }
