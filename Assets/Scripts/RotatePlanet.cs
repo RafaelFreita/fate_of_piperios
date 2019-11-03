efc1c54f7bd083bb942facb56a74a5ccafd52a7e
RafaelFreita <dfr.rafael@gmail.com>
2019-11-03 09:27:58 -0300

    Adding planet rotation script.

---
+++ Assets/Scripts/RotatePlanet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlanet : MonoBehaviour
+{
+
+    public float speed = 2.0f;
+
+    private float auxHorizontal;
+    private float auxVertical;
+
+    void Update()
+    {
+        auxVertical = Input.GetAxis("Vertical");
+        auxHorizontal = Input.GetAxis("Horizontal");
+
+        transform.Rotate(Vector3.right, auxVertical * speed * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.up, auxHorizontal * speed * Time.deltaTime, Space.World);
+    }
+}
