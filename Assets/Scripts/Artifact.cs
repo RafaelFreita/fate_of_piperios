3ec9af30eabf1d4fb7e451b19c6b2b252c8b430f
RafaelFreita <dfr.rafael@gmail.com>
2019-11-06 22:29:18 -0300

    adding random colors and screen fade

--- Assets/Scripts/Artifact.cs
+++ Assets/Scripts/Artifact.cs
@@ -8,14 +8,31 @@ using UnityEngine.SceneManagement;
 public class Artifact : MonoBehaviour
 {
 
+    static bool reachedEnd = false;
+
     public StoryNode node;
 
     private bool activatable = true;
     private TextMeshProUGUI textLog;
 
+    private Material material;
+    private Color artifactColor;
+
+    private void Awake()
+    {
+        reachedEnd = false;
+    }
+
     private void Start()
     {
         textLog = GameObject.FindGameObjectWithTag("HistoryLog").GetComponent<TextMeshProUGUI>();
+
+        material = GetComponent<MeshRenderer>().material;
+
+        artifactColor = Random.ColorHSV(0f, 1f, .5f, 1f, .45f, 0.7f, 1f, 1f);
+
+        material.SetColor("_BaseColor", artifactColor);
+        material.SetColor("_EmissionColor", artifactColor);
     }
 
     private void Activate()
@@ -35,6 +52,7 @@ public class Artifact : MonoBehaviour
             if (node.endRule.Check()) // Mundo acabou
             {
                 textLog.text = node.endRule.triggeredMessage;
+                reachedEnd = true;
                 StartCoroutine(RestartSceneAsync());
             }
             else                      // Mundo não acabou
@@ -53,14 +71,20 @@ public class Artifact : MonoBehaviour
 
     IEnumerator RestartSceneAsync()
     {
+        GameObject.FindWithTag("LevelChanger").GetComponent<Animator>().SetTrigger("FadeOut");
+
         yield return new WaitForSeconds(6);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     private void OnMouseDown()
     {
+        if (reachedEnd) return;
         Debug.Log("Clicked on artifact: " + name);
         Activate();
+
+        artifactColor = artifactColor * 3;
+        material.SetColor("_EmissionColor", artifactColor);
     }
 
 }
