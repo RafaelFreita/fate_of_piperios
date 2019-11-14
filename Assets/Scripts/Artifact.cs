dae1916e2cdb68b11c5d2d91eccb72389d46c40f
Rafael de Freitas <jamerrf13@unisinos.br>
2019-11-14 13:05:00 -0300

    Adding better text log; Uggly af

--- Assets/Scripts/Artifact.cs
+++ Assets/Scripts/Artifact.cs
@@ -1,90 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
-
-using UnityEngine.SceneManagement;
-
 public class Artifact : MonoBehaviour
 {
-
-    static bool reachedEnd = false;
 
     public StoryNode node;
 
     private bool activatable = true;
-    private TextMeshProUGUI textLog;
-
-    private Material material;
-    private Color artifactColor;
-
-    private void Awake()
-    {
-        reachedEnd = false;
-    }
+    private LogManager logManager;
 
     private void Start()
     {
-        textLog = GameObject.FindGameObjectWithTag("HistoryLog").GetComponent<TextMeshProUGUI>();
-
-        material = GetComponent<MeshRenderer>().material;
-
-        artifactColor = Random.ColorHSV(0f, 1f, .5f, 1f, .45f, 0.7f, 1f, 1f);
-
-        material.SetColor("_BaseColor", artifactColor);
-        material.SetColor("_EmissionColor", artifactColor);
+        logManager = GameObject.FindGameObjectWithTag("HistoryLog").GetComponent<LogManager>();
     }
 
     private void Activate()
     {
-        // Não ser clicável caso já tenha passado por esse nodo
-        if (node.isActive) return;
+        // Não ser clicável caso o mundo não acabe nesse path
+        if (!activatable) return;
 
         // Ativa o nodo
         node.isActive = true;
 
-        // Mostra a mensagem
-        textLog.text = node.message;
-        
+        bool textAdded = false;
+
         // Checa se possui end rule
         if (node.endRule)
         {
+            textAdded = true;
             if (node.endRule.Check()) // Mundo acabou
             {
-                textLog.text = node.endRule.triggeredMessage;
-                reachedEnd = true;
-                StartCoroutine(RestartSceneAsync());
+                logManager.AddMessage(node.endRule.triggeredMessage);
             }
             else                      // Mundo não acabou
             {
-                textLog.text = node.endRule.untriggeredMessage;
+                logManager.AddMessage(node.endRule.untriggeredMessage);
+                activatable = false;
             }
+        }
+
+        if (!textAdded)
+        {
+            // Mostra a mensagem
+            logManager.AddMessage(node.message);
         }
 
         // Passa pro proximo nodo
         if (node.nextNode)
             node = node.nextNode;
         else
-            Debug.Log("Didn't find next node!! Title: " + node.title);
+            Debug.LogError("Didn't find next node!! Title: " + node.title);
 
-    }
-
-    IEnumerator RestartSceneAsync()
-    {
-        GameObject.FindWithTag("LevelChanger").GetComponent<Animator>().SetTrigger("FadeOut");
-
-        yield return new WaitForSeconds(6);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     private void OnMouseDown()
     {
-        if (reachedEnd) return;
         Debug.Log("Clicked on artifact: " + name);
         Activate();
-
-        artifactColor = artifactColor * 3;
-        material.SetColor("_EmissionColor", artifactColor);
     }
 
 }
---
+++ Assets/Scripts/LogManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LogManager : MonoBehaviour
+{
+
+    public GameObject logEntry;
+
+    public void AddMessage(string text)
+    {
+        TextMeshProUGUI textMesh = Instantiate(logEntry, transform).GetComponent<TextMeshProUGUI>();
+        textMesh.text = text;
+    }
+
+}
