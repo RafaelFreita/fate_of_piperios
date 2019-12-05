08c03b872c7c8017a6a1e58f967ac219e0d0ffa4
RafaelFreita <dfr.rafael@gmail.com>
2019-12-04 21:26:55 -0300

    Adding song; Adding fov explosion;

--- Assets/Scripts/ScrollController.cs
+++ Assets/Scripts/ScrollController.cs
@@ -8,14 +8,35 @@ public class ScrollController : MonoBehaviour
 
     private ScrollRect scroll;
 
+    private Camera cameraMain;
+    public AnimationCurve curve;
+
     private void Awake()
     {
         scroll = GetComponent<ScrollRect>();
+        cameraMain = Camera.main;
     }
 
     public void MoveToBottom()
     {
+        Debug.Log("Move to bottom!");
         StartCoroutine(WaitAndMove());
+        StartCoroutine(FovExplosion(0.3f));
+    }
+
+    IEnumerator FovExplosion(float duration) {
+        Debug.Log("Fov explosion!");
+        float t = 0.0f;
+
+        float s = 1.0f / duration;
+        while (t < 1.0f)
+        {
+            t += Time.deltaTime * s;
+            t = Mathf.Clamp01(t);
+            cameraMain.fieldOfView = curve.Evaluate(t);
+            yield return null;
+        }
+
     }
 
     IEnumerator WaitAndMove()
@@ -23,7 +44,7 @@ public class ScrollController : MonoBehaviour
         yield return new WaitForEndOfFrame();
 
         Canvas.ForceUpdateCanvases();
-        scroll.verticalNormalizedPosition = 0.0f;
+        scroll.horizontalNormalizedPosition = 1.0f;
         Canvas.ForceUpdateCanvases();
     }
 
